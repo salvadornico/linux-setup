64b44d6789bedd10170ae921e8629a52977b1072
Nico Salvador <salvador.nico@gmail.com>
2020-06-13 12:35:28 +0800

    Separate into steps

--- step2.sh
+++ step2.sh
@@ -1,16 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-# Basic setup
-sudo apt update
-
-# ZSH
-sudo apt install -y zsh
-zsh --version
-chsh -s $(which zsh)
-echo $SHELL
-
-# Oh My ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
