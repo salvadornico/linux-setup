9d9096d1ef4e3489de7e526c3c28c31bbcedb0d1
Nico Salvador <salvador.nico@gmail.com>
2020-06-13 12:31:10 +0800

    Add zsh plugins

--- setup.sh
+++ setup.sh
@@ -4,10 +4,20 @@
 sudo apt update
 
 # ZSH
-sudo apt install zsh
+sudo apt install -y zsh
 zsh --version
 chsh -s $(which zsh)
 echo $SHELL
 
 # Oh My ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="spaceship"/g' ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g' ~/.zshrc
+
+# git setup
+git config credential.helper store
